a20ec708029bcf64e89978b0e7fbf8ea48682e99
Jana Moudrá <jana.moudra@gmail.com>
2017-04-29 11:08:59 +0200

    Add step 3

---
+++ 3-yourfirstcomponent/firebase_counter/lib/app_component.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2017, janamou. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+import 'package:angular2_components/angular2_components.dart';
+
+import 'hello_dialog/hello_dialog.dart';
+
+@Component(
+  selector: 'my-app',
+  styleUrls: const ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: const [materialDirectives, HelloDialog],
+  providers: const [materialProviders],
+)
+class AppComponent {
+  int count = 0;
+}
