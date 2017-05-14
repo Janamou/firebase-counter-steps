f8e2b9f3f96a0b6431f3abf190118b77387a4ec2
Jana Moudrá <jana.moudra@gmail.com>
2017-05-15 01:05:06 +0200

    Update step 4

--- 4-loaddatafromfirebase/firebase_counter/lib/app_component.dart
+++ 4-loaddatafromfirebase/firebase_counter/lib/app_component.dart
@@ -2,27 +2,36 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/core.dart';
-import 'package:angular2_components/angular2_components.dart';
+import 'package:angular_components/angular_components.dart';
 import 'package:firebase/firebase.dart';
-import 'hello_dialog/hello_dialog.dart';
+
+import 'todo_list/todo_list_component.dart';
+
+// AngularDart info: https://webdev.dartlang.org/angular
+// Components info: https://webdev.dartlang.org/components
 
 @Component(
   selector: 'my-app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [materialDirectives, HelloDialog],
+  directives: const [materialDirectives, TodoListComponent],
   providers: const [materialProviders],
 )
 class AppComponent implements OnInit {
   int count = 0;
-  DatabaseReference _ref;
+  DatabaseReference ref;
 
   @override
   ngOnInit() {
-    _ref = database().ref('counter');
-    _ref.onValue.listen((e) {
-      DataSnapshot snapshot = e.snapshot;
-      count = snapshot.val();
+    initializeApp(
+        apiKey: "AIzaSyAH7S_gsce9RtNI8w0z7doiP3ugVJM8ZbI",
+        authDomain: "angulardart-firebase-io-2017.firebaseapp.com",
+        databaseURL: "https://angulardart-firebase-io-2017.firebaseio.com",
+        storageBucket: "angulardart-firebase-io-2017.appspot.com");
+
+    ref = database().ref('counter');
+    ref.onValue.listen((e) {
+      count = e.snapshot.val();
     });
   }
 }
