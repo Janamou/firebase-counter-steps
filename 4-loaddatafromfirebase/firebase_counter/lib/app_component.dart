812adbd2ba1fc7f341f74ba3bb8e91bea7589fd5
Jana Moudrá <jana.moudra@gmail.com>
2017-09-15 00:32:57 +0200

    Step 4 update

--- 4-loaddatafromfirebase/firebase_counter/lib/app_component.dart
+++ 4-loaddatafromfirebase/firebase_counter/lib/app_component.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2017, janamou. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:angular2/angular2.dart';
+import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
 import 'package:firebase/firebase.dart';
 
